77d9b82040109efef4f5e2aa10e8691c126f42b4
Om Chouksey <omchouksey27@gmail.com>
2026-05-24 16:45:45 +0530

    fix cors.js

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,5 +1,29 @@
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Disposition'],
+  credentials: false,
+};
+
+module.exports = corsOptions;
+
+
 // const corsOptions = {
-//   origin: 'http://localhost:5173',
+//   origin: function (origin, callback) {
+//     const allowed = [
+//       'http://localhost:5173',
+//       'http://localhost:3000',
+//       process.env.FRONTEND_URL,
+//       'https://gram-meet.vercel.app/login', // your actual Vercel URL
+//     ].filter(Boolean);
+
+//     if (!origin || allowed.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
 //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 //   allowedHeaders: ['Content-Type', 'Authorization'],
 //   exposedHeaders: ['Content-Disposition'],
@@ -7,27 +31,3 @@
 // };
 
 // module.exports = corsOptions;
-
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowed = [
-      'http://localhost:5173',
-      'http://localhost:3000',
-      process.env.FRONTEND_URL,
-      'https://gram-meet.vercel.app/login', // your actual Vercel URL
-    ].filter(Boolean);
-
-    if (!origin || allowed.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['Content-Disposition'],
-  credentials: true,
-};
-
-module.exports = corsOptions;
